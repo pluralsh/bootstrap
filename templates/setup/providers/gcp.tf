5703039bf4f0cf5f42848a6ad7fcd5f1de40166e
Sebastian Florek <sebastian@plural.sh>
2024-11-26 12:53:52 +0100

    add passthrough variable to gcp tf

--- templates/setup/providers/gcp.tf
+++ templates/setup/providers/gcp.tf
@@ -1,7 +1,14 @@
+# Variable passthrough to the GCP module in order
+# to enable TF_VAR_xxx environment variable usage.
+variable "network" { type = string }
+variable "subnetwork" { type = string }
+
 module "mgmt" {
     source        = "./cluster"
     project_id    = "{{ .Project }}"
     cluster_name  = "{{ .Cluster }}"
     region        = "{{ .Region }}"
     create_db     = {{ .RequireDB }}
+    network       = "${var.network}"
+    subnetwork    = "${var.subnetwork}"
 }
