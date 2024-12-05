c382cb2a40298acab5848029bd8f49f1c6e6633a
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-12-05 11:23:12 +0100

    add variable deletion_protection

--- templates/setup/providers/aws.tf
+++ templates/setup/providers/aws.tf
@@ -1,5 +1,12 @@
+variable "deletion_protection" {
+    type    = bool
+    default = true
+}
+
 module "mgmt" {
     source        = "./cluster"
     cluster_name  = "{{ .Cluster }}"
     create_db     = {{ .RequireDB }}
+    deletion_protection = "${var.deletion_protection}"
 }
+
