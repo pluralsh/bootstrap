473916b63ac3996a3114f8d69699d8afb12e2b29
michaeljguarino <mjg@plural.sh>
2025-01-08 11:25:58 -0500

    Fix cluster name variable

--- terraform/modules/clusters/aws/context.tf
+++ terraform/modules/clusters/aws/context.tf
@@ -1,9 +1,9 @@
 resource "plural_service_context" "mgmt" {
-    name = "plrl/clusters/${var.cluster_name}"
+    name = "plrl/clusters/${var.cluster}"
 
     configuration = jsonencode({
         region          = var.region
-        cluster_name    = var.cluster_name
+        cluster_name    = var.cluster
         vpc_id          = module.vpc.vpc_id
         subnet_ids      = concat(module.vpc.public_subnets, module.vpc.private_subnets)
         private_subnets = module.vpc.private_subnets
--- terraform/modules/clusters/gcp/context.tf
+++ terraform/modules/clusters/gcp/context.tf
@@ -1,9 +1,9 @@
 resource "plural_service_context" "mgmt" {
-    name = "plrl/clusters/${var.cluster_name}"
+    name = "plrl/clusters/${var.cluster}"
 
     configuration = jsonencode({
         region       = var.region
-        cluster_name = var.cluster_name
+        cluster_name = var.cluster
         network      = module.gcp-network.network_name
         subnetwork   = module.gcp-network.subnets_names[0]
         cidr         = var.subnet_cidr
