8547e327d213735c8b2954c15080008a8a95ea17
michaeljguarino <mjg@plural.sh>
2025-03-25 17:48:35 -0400

    fix tf wiring bug in service context

--- terraform/modules/clusters/aws/context.tf
+++ terraform/modules/clusters/aws/context.tf
@@ -1,13 +1,13 @@
-resource "plural_service_context" "mgmt" {
+resource "plural_service_context" "cluster" {
     name = "plrl/clusters/${var.cluster}"
 
     configuration = jsonencode({
         region          = var.region
         cluster_name    = var.cluster
-        vpc_id          = module.vpc.vpc_id
-        subnet_ids      = concat(module.vpc.public_subnets, module.vpc.private_subnets)
-        private_subnets = module.vpc.private_subnets
-        public_subnets  = module.vpc.public_subnets
+        vpc_id          = local.vpc.vpc_id
+        subnet_ids      = concat(local.vpc.private_subnets, local.vpc.public_subnets)
+        private_subnets = local.vpc.private_subnets
+        public_subnets  = local.vpc.public_subnets
         vpc_cidr        = var.vpc_cidr
     })
 }
