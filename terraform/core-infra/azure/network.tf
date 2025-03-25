a25af411a7c5efd5db26d553781cb9e1adf05d07
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-03-25 12:45:00 +0100

    create dev and prod networks

--- terraform/core-infra/azure/network.tf
+++ terraform/core-infra/azure/network.tf
@@ -1,28 +1,124 @@
-data "azurerm_virtual_network" "plural" {
-  name = var.network_name
-  resource_group_name = var.project
+data "azurerm_resource_group" "default" {
+  name = var.project
 }
 
-data "azurerm_subnet" "pg" {
-  name = var.pg_subnet_name
-  virtual_network_name = var.network_name
-  resource_group_name = var.project
+resource "azurerm_virtual_network" "dev" {
+  name                = "dev"
+  resource_group_name = data.azurerm_resource_group.default.name
+  location            = var.region
+  address_space       = var.network_cidrs
 }
 
-data "azurerm_private_dns_zone" "default" {
-  name = var.dns_zone_name
-  resource_group_name = var.project
+resource "azurerm_subnet" "dev_sn" {
+  name                                           = "dev-sn"
+  resource_group_name                            = data.azurerm_resource_group.default.name
+  virtual_network_name                           = azurerm_virtual_network.dev.name
+  address_prefixes                               = var.subnet_cidrs
+  enforce_private_link_endpoint_network_policies = true
 }
 
-resource "plural_service_context" "pg" {
-  name = "plrl/subnets/pg"
+resource "azurerm_subnet" "dev_pg" {
+  name                 = "dev-pg"
+  resource_group_name  = data.azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.dev.name
+  address_prefixes     = var.postgres_cidrs
+  service_endpoints = ["Microsoft.Storage"]
+
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_private_dns_zone" "dev_pg" {
+  name                = var.postgres_dns_zone
+  resource_group_name = data.azurerm_resource_group.default.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dev_pg" {
+  name                  = var.network_link_name
+  private_dns_zone_name = azurerm_private_dns_zone.dev_pg.name
+  virtual_network_id    = azurerm_virtual_network.dev.id
+  resource_group_name   = data.azurerm_resource_group.default.name
+}
+
+resource "plural_service_context" "dev" {
+  name = "plrl/network/dev"
   configuration = jsonencode({
-    region        = var.region
-    network_name  = var.network_name
-    network_id    = data.azurerm_virtual_network.plural.id
-    subnet_name   = var.pg_subnet_name
-    subnet_id     = data.azurerm_subnet.pg.id
-    dns_zone_name = var.dns_zone_name
-    dns_zone_id   = data.azurerm_private_dns_zone.default.id
+    location        = azurerm_virtual_network.dev.location
+    network_name  = azurerm_virtual_network.dev.name
+    network_id    = azurerm_virtual_network.dev.id
+    sn_subnet_name   = azurerm_subnet.dev_sn.name
+    sn_subnet_id     = azurerm_subnet.dev_sn.id
+    pg_subnet_name   = azurerm_subnet.dev_pg.name
+    pg_subnet_id     = azurerm_subnet.dev_pg.id
+    dns_zone_name = azurerm_private_dns_zone.dev_pg.name
+    dns_zone_id   = azurerm_private_dns_zone.dev_pg.id
   })
 }
+
+resource "azurerm_virtual_network" "prod" {
+  name                = "prod"
+  resource_group_name = data.azurerm_resource_group.default.name
+  location            = var.region
+  address_space       = var.network_cidrs
+}
+
+resource "azurerm_subnet" "prod_sn" {
+  name                                           = "prod-sn"
+  resource_group_name                            = data.azurerm_resource_group.default.name
+  virtual_network_name                           = azurerm_virtual_network.prod.name
+  address_prefixes                               = var.subnet_cidrs
+  enforce_private_link_endpoint_network_policies = true
+}
+
+resource "azurerm_subnet" "prod_pg" {
+  name                 = "prod-pg"
+  resource_group_name  = data.azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.prod.name
+  address_prefixes     = var.postgres_cidrs
+  service_endpoints = ["Microsoft.Storage"]
+
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_private_dns_zone" "prod_pg" {
+  name                = var.postgres_dns_zone
+  resource_group_name = data.azurerm_resource_group.default.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "prod_pg" {
+  name                  = var.network_link_name
+  private_dns_zone_name = azurerm_private_dns_zone.prod_pg.name
+  virtual_network_id    = azurerm_virtual_network.prod.id
+  resource_group_name   = data.azurerm_resource_group.default.name
+}
+
+resource "plural_service_context" "prod" {
+  name = "plrl/network/prod"
+  configuration = jsonencode({
+    location        = azurerm_virtual_network.prod.location
+    network_name  = azurerm_virtual_network.prod.name
+    network_id    = azurerm_virtual_network.prod.id
+    sn_subnet_name   = azurerm_subnet.prod_sn.name
+    sn_subnet_id     = azurerm_subnet.prod_sn.id
+    pg_subnet_name   = azurerm_subnet.prod_pg.name
+    pg_subnet_id     = azurerm_subnet.prod_pg.id
+    dns_zone_name = azurerm_private_dns_zone.prod_pg.name
+    dns_zone_id   = azurerm_private_dns_zone.prod_pg.id
+  })
+}
+
--- terraform/core-infra/azure/variables.tf
+++ terraform/core-infra/azure/variables.tf
@@ -1,3 +1,27 @@
+
+variable "network_cidrs" {
+  type = list(string)
+  default = ["10.52.0.0/16"]
+}
+
+variable "subnet_cidrs" {
+  type = list(string)
+  default = ["10.52.0.0/20"]
+}
+
+variable "postgres_cidrs" {
+  type = list(string)
+  default = ["10.52.16.0/24"]
+}
+
+variable "postgres_dns_zone" {
+  default = "plrl.postgres.database.azure.com"
+}
+
+variable "network_link_name" {
+  default = "plrl.postgres.com"
+}
+
 variable "region" {
   type = string
   default = "us-east-2"
