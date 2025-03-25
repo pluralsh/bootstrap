866f6a85b6be6f0d0fb9b3456a8eb3dca6df1cb3
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-03-25 14:07:02 +0100

    add missing provider

--- terraform/clouds/azure/providers.tf
+++ terraform/clouds/azure/providers.tf
@@ -6,5 +6,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=3.51.0, < 4.0"
     }
+    plural = {
+      source = "pluralsh/plural"
+      version = ">= 0.2.16"
+    }
   }
 }
