595b703fb274a6ea48b7937f1bf0b588ed63696a
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-04-07 11:18:27 +0200

    initial aks setup

---
+++ terraform/modules/clusters/azure/plural.tf
@@ -0,0 +1,34 @@
+data "plural_service_context" "identity" {
+  name = "plrl/azure/identity"
+}
+
+data "plural_service_context" "network" {
+  name = "plrl/network/${var.tier}"
+}
+
+resource "plural_service_context" "cluster" {
+  name = "plrl/clusters/${var.cluster}"
+
+  configuration = jsonencode({
+    region       = var.region
+    cluster_name = var.cluster
+  })
+}
+
+resource "plural_cluster" "cluster" {
+    handle = var.cluster
+    name   = var.cluster
+  
+    tags   = {
+      tier = var.tier
+      fleet = var.fleet
+      role = "workload"
+    }
+
+    kubeconfig = {
+      host =  module.aks.cluster_fqdn
+      cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+      client_certificate     = base64decode(module.aks.client_certificate)
+      client_key             = base64decode(module.aks.client_key)
+    }
+}
