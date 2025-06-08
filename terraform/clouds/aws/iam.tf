3d5a02f0bf69d1aab560d02dcf67f68780cfff45
michaeljguarino <mjg@plural.sh>
2025-06-07 21:53:49 -0400

    add externaldns/cert manager configs

--- terraform/clouds/aws/iam.tf
+++ terraform/clouds/aws/iam.tf
@@ -154,3 +154,135 @@ resource "aws_eks_pod_identity_association" "cloudwatch_pod_identity" {
   service_account = "cloudwatch-exporter"
   role_arn        = aws_iam_role.cloudwatch_exporter_role.arn
 }
+
+module "externaldns_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.33"
+
+  role_name                  = "${var.cluster_name}-externaldns"
+  attach_external_dns_policy = true
+  attach_cert_manager_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["external-dns:external-dns", "cert-manager:cert-manager"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "external_dns" {
+  statement {
+    actions   = ["route53:ChangeResourceRecordSets"]
+    resources = ["arn:aws:route53:::hostedzone/*"]
+  }
+
+  statement {
+    actions = [
+      "route53:ListHostedZones",
+      "route53:ListResourceRecordSets",
+      "route53:ListTagsForResources",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "cert_manager" {
+  statement {
+    actions   = ["route53:GetChange"]
+    resources = ["arn:aws:route53:::change/*"]
+  }
+
+  statement {
+    actions = [
+      "route53:ChangeResourceRecordSets",
+      "route53:ListResourceRecordSets"
+    ]
+
+    resources = ["arn:aws:route53:::hostedzone/*"]
+  }
+
+  statement {
+    actions   = ["route53:ListHostedZonesByName"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "external_dns_role" {
+  name = "${var.cluster_name}-external-dns"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowEksAuthToAssumeRoleForPodIdentity"
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "external_dns" {
+  name_prefix = "external-dns"
+  description = "external dns permissions for ${var.cluster_name}"
+  policy      = data.aws_iam_policy_document.external_dns.json
+}
+
+resource "aws_iam_role_policy_attachment" "external_dns_policy_attach" {
+  role       = aws_iam_role.external_dns_role.name
+  policy_arn = aws_iam_policy.external_dns.arn
+}
+
+resource "aws_iam_role" "cert_manager_role" {
+  name = "${var.cluster_name}-cert-manager"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowEksAuthToAssumeRoleForPodIdentity"
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "cert_manager" {
+  name_prefix = "cert-manager"
+  description = "cert manager permissions for ${var.cluster_name}"
+  policy      = data.aws_iam_policy_document.cert_manager.json
+}
+
+resource "aws_iam_role_policy_attachment" "cert_manager_policy_attach" {
+  role       = aws_iam_role.cert_manager_role.name
+  policy_arn = aws_iam_policy.cert_manager.arn
+}
+
+resource "aws_eks_pod_identity_association" "cert_manager_pod_identity" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "cert-manager"
+  service_account = "cert-manager"
+  role_arn        = aws_iam_role.cert_manager_role.arn
+}
+
+resource "aws_eks_pod_identity_association" "external_dns_pod_identity" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "external-dns"
+  service_account = "external-dns"
+  role_arn        = aws_iam_role.external_dns_role.arn
+}
