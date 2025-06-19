c3ecc181e79c1245b9f5323ec609bd9e50bf799c
michaeljguarino <mjg@plural.sh>
2025-06-19 17:41:56 -0400

    fix aws bootstrap version

--- templates/providers/bootstrap/aws.tf
+++ templates/providers/bootstrap/aws.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.57"
+      version = "< 6.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
