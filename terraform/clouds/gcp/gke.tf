05ff211bbb686873b3fe64c54a776f88034e0066
Sebastian Florek <s.florek91@gmail.com>
2025-07-16 11:33:28 +0200

    feat(gke): implement blue-green deployment for management clusters

--- terraform/clouds/gcp/gke.tf
+++ terraform/clouds/gcp/gke.tf
@@ -1,8 +1,34 @@
+locals {
+  node_pool_add = {
+    (local.active_node_group) = {
+      cluster_version = var.kubernetes_version,
+    },
+    (local.drain_node_group) = {
+      cluster_version = var.next_kubernetes_version,
+    }
+  }
+
+  full_node_pools = {
+    for k, v in var.node_pools : k => merge(v, try(lookup(local.node_pool_add, k), {}))
+    if k != local.drain_node_group || local.upgrading == true
+  }
+
+  node_pool_taints_add = {
+    (local.active_node_group) = local.upgrading ?
+      [{ key = "platform.plural.sh/pending", value = "upgrade", effect = "NO_SCHEDULE" }] : [],
+  }
+
+  full_node_pools_taints = {
+    for k, v in var.node_pools_taints : k => concat(v, try(lookup(local.node_pool_taints_add, k), []))
+    if k != local.drain_node_group || local.upgrading == true
+  }
+}
+
 module "gke" {
   source  = "terraform-google-modules/kubernetes-engine/google"
   version = "~> 33.0"
 
-  kubernetes_version     = var.kubernetes_version
+  kubernetes_version = var.next_kubernetes_version
   project_id             = var.project_id
   name                   = var.cluster_name
   regional               = true
@@ -14,8 +40,8 @@ module "gke" {
   ip_range_services      = var.ip_range_services_name
   create_service_account = true
   deletion_protection    = var.deletion_protection
-  node_pools             = var.node_pools
-  node_pools_taints      = var.node_pools_taints
+  node_pools         = [for k, v in local.full_node_pools : merge(v, { name = k })]
+  node_pools_taints  = local.full_node_pools_taints
   node_pools_labels      = var.node_pools_labels
   node_pools_tags        = var.node_pools_tags
 
@@ -27,6 +53,5 @@ module "gke" {
     google_project_service.iam,
     google_project_service.storage,
     google_project_service.dns,
-    # local.db_created,
   ]
 }
