581caceaa10d6c82325ee17c23656ef0126ab9d3
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-02-11 14:03:16 +0100

    update console Helm chart version to 0.3.139

--- templates/setup/console.tf
+++ templates/setup/console.tf
@@ -76,7 +76,7 @@ resource "helm_release" "console" {
   namespace        = "plrl-console"
   chart            = "console"
   repository       = "https://pluralsh.github.io/console"
-  version          = "0.3.125"
+  version          = "0.3.139"
   create_namespace = true
   timeout          = 600
   wait             = true
